0d0a63e771ef737b0ca6299df50356bf26807703
aleeevola <aleeevola@gmail.com>
2019-10-28 00:21:39 -0300

    Todo andando

---
+++ BasesDeDatos.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: licencias
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `licencias`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `licencias` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `licencias`;
+
+--
+-- Table structure for table `licencia`
+--
+
+DROP TABLE IF EXISTS `licencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `licencia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `clase` varchar(1) NOT NULL,
+  `fecha_de_vencimiento` date DEFAULT NULL,
+  `numero_de_copias` int(11) DEFAULT NULL,
+  `titular_id` int(11) DEFAULT NULL,
+  `emitidoPor` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `emitidoPor` (`emitidoPor`),
+  KEY `titular_id` (`titular_id`),
+  CONSTRAINT `licencia_ibfk_1` FOREIGN KEY (`emitidoPor`) REFERENCES `usuarioadministrador` (`usuario`),
+  CONSTRAINT `licencia_ibfk_2` FOREIGN KEY (`titular_id`) REFERENCES `titular` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `licencia`
+--
+
+LOCK TABLES `licencia` WRITE;
+/*!40000 ALTER TABLE `licencia` DISABLE KEYS */;
+INSERT INTO `licencia` VALUES (1,'D','1997-02-03',3,1,'aleeevola');
+/*!40000 ALTER TABLE `licencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `titular`
+--
+
+DROP TABLE IF EXISTS `titular`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `titular` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `fecha_de_nacimiento` date DEFAULT NULL,
+  `direccion` varchar(40) DEFAULT NULL,
+  `sangre` varchar(15) DEFAULT NULL,
+  `donante_de_organos` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`,`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `titular`
+--
+
+LOCK TABLES `titular` WRITE;
+/*!40000 ALTER TABLE `titular` DISABLE KEYS */;
+INSERT INTO `titular` VALUES (1,40450769,'Raviolo','Josefina','1997-02-03','SANTOKIO','A_NEGATIVO',1);
+/*!40000 ALTER TABLE `titular` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarioadministrador`
+--
+
+DROP TABLE IF EXISTS `usuarioadministrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarioadministrador` (
+  `usuario` varchar(30) NOT NULL,
+  PRIMARY KEY (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarioadministrador`
+--
+
+LOCK TABLES `usuarioadministrador` WRITE;
+/*!40000 ALTER TABLE `usuarioadministrador` DISABLE KEYS */;
+INSERT INTO `usuarioadministrador` VALUES ('aleeevola');
+/*!40000 ALTER TABLE `usuarioadministrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `registrocivil`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `registrocivil` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `registrocivil`;
+
+--
+-- Table structure for table `personas`
+--
+
+DROP TABLE IF EXISTS `personas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `fecha_de_nacimiento` date DEFAULT NULL,
+  `direccion` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`,`dni`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personas`
+--
+
+LOCK TABLES `personas` WRITE;
+/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
+INSERT INTO `personas` VALUES (1,40450769,'Vola','Alejandro Bruno','1997-06-07','Norte'),(2,40450769,'Perez','Esteban','2000-10-20','9 de julio 1720'),(3,180549123,'Rodiguez','Marcos','1978-12-24','Junin 1253');
+/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-28  0:19:09
