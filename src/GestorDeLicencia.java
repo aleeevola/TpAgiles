af6605b5edb54dfa0083404dda6a92baa57fcf36
Jraviolo <josefina.raviolo@gmail.com>
2019-11-02 19:19:24 -0300

    H001-T02 logica

--- src/GestorDeLicencia.java
+++ src/GestorDeLicencia.java
@@ -3,13 +3,70 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import clases.Clase;
 import clases.Licencia;
+import clases.Titular;
 
 public class GestorDeLicencia {
 
 	public GestorDeLicencia() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Boolean emitirLicencia(Clase clase,int dni, Date fecha_de_nacimiento, String nombre, String apellido) {
+		GestorBaseDeDatos bd= new GestorBaseDeDatos();
+		
+		GregorianCalendar CalendarFecha_de_nacimiento = new GregorianCalendar();
+		CalendarFecha_de_nacimiento.setTime(fecha_de_nacimiento);
+		
+		Integer edad=this._calcularEdad(CalendarFecha_de_nacimiento);
+		
+		if (edad>=17) {
+		
+		ArrayList<Titular> titulares=bd.getTitular(dni, apellido, nombre);
+		Date fecha_de_vencimiento = new Date();
+		
+		if(titulares.isEmpty()) {
+			System.out.println("No existe");
+			
+			if(clase==Clase.A || clase==Clase.B || clase==Clase.G) {
+					fecha_de_vencimiento=this.calcularVigencia(fecha_de_nacimiento, null);
+					
+					Licencia licencia = new Licencia();
+					licencia.setClase(clase);
+					licencia.setNumero_de_copias(0);
+					licencia.setFecha_de_vencimiento(fecha_de_vencimiento);
+					
+					/*
+					 * Aca debe llamar a H009-T01  para crear un titular y pasarle la licencia
+					 */
+					return true;
+			} else return false;
+			
+			
+		}
+		else {
+			if((clase==Clase.C || clase==Clase.D || clase==Clase.E) && 16<edad && edad<66) {
+				
+			}
+			System.out.println("existe");
+			Titular titular=titulares.get(0);
+			
+			fecha_de_vencimiento=this.calcularVigencia(fecha_de_nacimiento, (ArrayList<Licencia>) titular.getLicencias());
+			
+			Licencia licencia = new Licencia();
+			licencia.setClase(clase);
+			licencia.setNumero_de_copias(0);
+			licencia.setFecha_de_vencimiento(fecha_de_vencimiento);
+			
+			titular.addLicencia(licencia);
+			
+			bd.guardarTitular(titular);
+			
+		}
+		} else return false;
+		return null;
 	}
 
 	public Date calcularVigencia(Date fechaNacimiento, ArrayList<Licencia> historialLicencias) {
